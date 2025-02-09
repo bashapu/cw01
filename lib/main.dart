88b19b2d2c27b996f3adb279a392c3b2839279ef
Bharath Ashapu <bashapu1@student.gsu.edu>
2025-02-09 16:11:28 -0500

    Switch bool value

--- lib/main.dart
+++ lib/main.dart
@@ -30,9 +30,9 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  final String _imageUrl_1 = 'https://i.natgeofe.com/n/8a4cd21e-3906-4c9d-8838-b13ef85f4b6e/tpc18-outdoor-gallery-1002418-12000351_01_square.jpg';
-  final String _imageUrl_2 = 'https://i.natgeofe.com/n/49f1c59b-095d-47a6-b72c-92bc6740a37c/tpc18-outdoor-gallery-1693450-12040196_03_square.jpg';
-  bool _imageFlag = false;
+  final String _imageUrl_1 = 'https://i.natgeofe.com/n/49f1c59b-095d-47a6-b72c-92bc6740a37c/tpc18-outdoor-gallery-1693450-12040196_03_square.jpg';
+  final String _imageUrl_2 = 'https://i.natgeofe.com/n/8a4cd21e-3906-4c9d-8838-b13ef85f4b6e/tpc18-outdoor-gallery-1002418-12000351_01_square.jpg';
+  bool _imageFlag = true;
 
   void _incrementCounter() {
     setState(() {
@@ -49,7 +49,7 @@ class _MyHomePageState extends State<MyHomePage> {
   void _resetApp() {
     setState(() {
       _counter = 0;
-      _imageFlag = false;
+      _imageFlag = true;
     });
   }
 
